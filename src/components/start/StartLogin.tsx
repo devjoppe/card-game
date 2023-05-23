4f65e01ee6e0b59aa449540e4af89fa339655740
Joakim Ottosson <joakim.ottosson@medieinstitutet.se>
2023-05-23 13:58:36 +0200

    Adding user to the db

--- src/components/start/StartLogin.tsx
+++ src/components/start/StartLogin.tsx
@@ -1,9 +1,32 @@
+import {useState} from "react";
+
+import { db } from '../../firebase/config.ts'
+import {collection, addDoc} from "firebase/firestore";
+
 const StartLogin = () => {
+
+    const [userName, setUserName] = useState('')
+
+    const saveUser = async () => {
+        console.log(userName)
+        if(userName) {
+            await addDoc(collection(db, 'highscore'), {
+                user: userName,
+                score: 0
+            })
+        }
+        return
+    }
+
     return(
         <div>
-            <form>
-                <input type="text" required placeholder="Enter username" />
-                <button type="submit">Start a new game</button>
+            <form onSubmit={(e) => {
+                e.preventDefault()
+                saveUser().then(() => {console.log("Is it done?")})
+                setUserName('')
+            }}>
+                <input type="text" value={userName} onChange={e => setUserName(e.target.value)} required placeholder="Enter username" />
+                <button type="submit" className="start">Start game</button>
             </form>
         </div>
     )
